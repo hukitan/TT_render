6587daeaed25adf8720a696c1b4e77db0cb9e585
Elias Mina <hukitan@hotmail.com>
2024-11-20 11:55:11 -0600

    Ya compila en render, hubo un error en la sintaxis no detectado y se corrije

--- app.py
+++ app.py
@@ -44,10 +44,9 @@ if start_button:
         pd.DataFrame(data=[[st.session_state['experiment_no'],
                             number_of_trials,
                             mean]],
-                     columns=['no', 'iterations', 'mean'])
+                    columns=['no', 'iterations', 'mean'])
         ],
         axis=0)
-    st.session_state['df_experiment_results'] = 
-        st.session_state['df_experiment_results'].reset_index(drop=True)
+    st.session_state['df_experiment_results'] = st.session_state['df_experiment_results'].reset_index(drop=True)
 
 st.write(st.session_state['df_experiment_results'])
